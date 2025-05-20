e2d5c5d9af2220dbdcda12313382ed781ae7172e
Isaias56789 <133094974+Isaias56789@users.noreply.github.com>
2025-05-20 11:24:20 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,21 @@ import jwt
 from flask import Flask, request, jsonify
 from functools import wraps
 from datetime import datetime, date, timedelta
+from flask import g
+
+def get_db_connection():
+    if 'db_conn' not in g:
+        g.db_conn = mysql.connector.connect(**db_config)
+    return g.db_conn
+
+@app.teardown_appcontext
+def close_db_connection(exception):
+    db_conn = g.pop('db_conn', None)
+    if db_conn is not None and db_conn.is_connected():
+        db_conn.close()
+
+
+______________________________________________________________________
 
 
 # Crear un objeto timedelta
